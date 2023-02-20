2aa75789a6c4f420e87372910b63497190c4d905
stafe-box <stafe_box@outlook.com>
2023-02-21 00:39:17 +0500

    Добавьте файлы проекта.

---
+++ TCPClient/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using System.Threading;
+using Microsoft.UI.Composition.SystemBackdrops;
+using System.Runtime.InteropServices; // For DllImport
+using WinRT;
+using Windows.Globalization.Fonts;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using System.Diagnostics;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace TCPClient
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See below for implementation.
+        MicaController m_backdropController;
+        SystemBackdropConfiguration m_configurationSource;
+
+        public MainWindow()
+        {
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            this.InitializeComponent();
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
+            appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 300, Height = 500 });
+            TrySetSystemBackdrop();
+            //TODO: Задать текст на русском
+            Caption.Text = "Клиент";
+            MessageBox.PlaceholderText = "Введите сообщкение здесь";
+            IPBox.PlaceholderText = "Введите IP адрес";
+            myButton.Content = "Отправить";
+            ResultBox.Text = "Ответ";
+        }
+
+        private async void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            string ip = IPBox.Text;
+            string msg = MessageBox.Text;
+            myButton.IsEnabled = false;
+            Progress.IsActive = true;
+            Client client = new Client();
+            client.Notify += Client_Notify;
+            await Task.Run(() => 
+            {
+                client.Connect(ip, msg);
+            });
+        }
+
+        private void Client_Notify(string responce)
+        {
+            this.DispatcherQueue.TryEnqueue(new Microsoft.UI.Dispatching.DispatcherQueueHandler(() =>
+            {
+                ResultBox.Text = responce;
+                Progress.IsActive = false;
+                myButton.IsEnabled = true;
+            }));
+        }
+
+        bool TrySetSystemBackdrop()
+        {
+            if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
+            {
+                m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+                m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+                // Create the policy object.
+                m_configurationSource = new SystemBackdropConfiguration();
+                this.Activated += Window_Activated;
+                this.Closed += Window_Closed;
+                ((FrameworkElement)this.Content).ActualThemeChanged += Window_ThemeChanged;
+
+                // Initial configuration state.
+                m_configurationSource.IsInputActive = true;
+                SetConfigurationSourceTheme();
+
+                m_backdropController = new Microsoft.UI.Composition.SystemBackdrops.MicaController();
+
+                // Enable the system backdrop.
+                // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+                m_backdropController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+                m_backdropController.SetSystemBackdropConfiguration(m_configurationSource);
+                return true; // succeeded
+            }
+
+            return false; // Mica is not supported on this system
+        }
+
+        private void Window_Activated(object sender, Microsoft.UI.Xaml.WindowActivatedEventArgs args)
+        {
+            m_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+        }
+
+        private void Window_Closed(object sender, WindowEventArgs args)
+        {
+            // Make sure any Mica/Acrylic controller is disposed
+            // so it doesn't try to use this closed window.
+            if (m_backdropController != null)
+            {
+                m_backdropController.Dispose();
+                m_backdropController = null;
+            }
+            this.Activated -= Window_Activated;
+            m_configurationSource = null;
+        }
+
+        private void Window_ThemeChanged(FrameworkElement sender, object args)
+        {
+            if (m_configurationSource != null)
+            {
+                SetConfigurationSourceTheme();
+            }
+        }
+
+        private void SetConfigurationSourceTheme()
+        {
+            switch (((FrameworkElement)this.Content).ActualTheme)
+            {
+                case ElementTheme.Dark: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark; break;
+                case ElementTheme.Light: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light; break;
+                case ElementTheme.Default: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default; break;
+            }
+        }
+        private void FontsChanger()
+        {
+            var fonts = new Windows.Globalization.Fonts.LanguageFontGroup("ru-ru");
+            var traditionalDocumentFont = fonts.TraditionalDocumentFont;
+            var modernDocumentFont = fonts.ModernDocumentFont;
+
+            // Obtain two properties of the traditional document font.
+            var traditionalDocumentFontFontFamily = traditionalDocumentFont.FontFamily;   // "MS Mincho"
+            var traditionalDocumentFontScaleFactor = traditionalDocumentFont.ScaleFactor; // 100
+
+            // Obtain two properties of the modern document font.
+            var modernDocumentFontFontFamily = modernDocumentFont.FontFamily;             // "Meiryo"
+            var modernDocumentFontScaleFactor = modernDocumentFont.ScaleFactor;           // 90
+        }
+    }
+    class WindowsSystemDispatcherQueueHelper
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct DispatcherQueueOptions
+        {
+            internal int dwSize;
+            internal int threadType;
+            internal int apartmentType;
+        }
+
+        [DllImport("CoreMessaging.dll")]
+        private static extern int CreateDispatcherQueueController([In] DispatcherQueueOptions options, [In, Out, MarshalAs(UnmanagedType.IUnknown)] ref object dispatcherQueueController);
+
+        object m_dispatcherQueueController = null;
+        public void EnsureWindowsSystemDispatcherQueueController()
+        {
+            if (Windows.System.DispatcherQueue.GetForCurrentThread() != null)
+            {
+                // one already exists, so we'll just use it.
+                return;
+            }
+
+            if (m_dispatcherQueueController == null)
+            {
+                DispatcherQueueOptions options;
+                options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+                options.threadType = 2;    // DQTYPE_THREAD_CURRENT
+                options.apartmentType = 2; // DQTAT_COM_STA
+
+                CreateDispatcherQueueController(options, ref m_dispatcherQueueController);
+            }
+        }
+    }
+}
